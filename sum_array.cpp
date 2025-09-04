8ecc0b5a192bfada05202a1cb1533dd8d3f7db8b
me250003021-spec <me250003021@iiti.ac.in>
2025-09-04 11:14:40 +0530

    Update sum_array.cpp

--- sum_array.cpp
+++ sum_array.cpp
@@ -3,17 +3,24 @@
 using namespace std;
 
 int sumArray(const vector<int>& arr) {
-    // TODO: complete the function as per instructions
+    int sum = 0;
+    for (int val : arr) {
+    sum += val;
+    }
+    return sum;
+    }
     
-}
 
 int main() {
-    int n; cin >> n;
+    int n;
+    cout << "Enter number of elements: ";
+     cin >> n;
     vector<int> arr(n);
-    for (int i=0; i<n; i++)
-        cin >> arr[i];
-
-    cout << sumArray(arr) << "\n";
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+    }
+    int result = sumArray(arr);
+    cout << "Sum of array elements = " << result << endl;
     return 0;
 }
-
